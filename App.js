eb20140134e8dfcddb021a98e525ca4b46e5d6de
mayank8005 <mayank8005@gmail.com>
2018-01-17 01:39:46 +0530

    added status bar

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { Constants } from 'expo'
+import { Black, LightWhite } from "./Utils/Colors";
+
+function AppStatus({backgroundColor, ...props}) {
+    return(
+        <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+            <StatusBar backgroundColor={backgroundColor} translucent {...props}/>
+        </View>
+    );
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+          <AppStatus backgroundColor={Black} barStyle='light-content'/>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Changes you make will automatically reload.</Text>
+            <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
   }
@@ -16,8 +27,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: LightWhite
   },
 });
---
+++ Utils/Colors.js
@@ -0,0 +1,4 @@
+export const Gray = '#303030';
+export const White = '#ffffff';
+export const Black = '#000000';
+export const LightWhite = '#FAFAFA';
