b68877149e54698cee91dee66b8b79c93460b8c3
mayank8005 <mayank8005@gmail.com>
2018-01-17 10:14:55 +0530

    change in action creator

--- actions/index.js
+++ actions/index.js
@@ -4,7 +4,7 @@ export const ADD_DECK = 'ADD_DECKS';
 export const ADD_CARD = 'ADD_CARD';
 
 //action creators
-export const loadDecksCreator = ()=>{return{type:LOAD_DECKS}};
+export const loadDecksCreator = (decks)=>{return{type:LOAD_DECKS, decks}};
 export const addDeckCreator = (deck)=>{return{type: ADD_DECK, deck}};
 export const addCardCreator = (deckId, card)=>{return{type: ADD_CARD, card, deckId}};
 
