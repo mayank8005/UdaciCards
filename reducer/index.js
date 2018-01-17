f9c49af05bdd63c233e42697681fc42072885063
mayank8005 <mayank8005@gmail.com>
2018-01-17 10:24:19 +0530

    added reducer

---
+++ reducer/index.js
@@ -0,0 +1,30 @@
+//importing actions
+import {LOAD_DECKS, ADD_CARD, ADD_DECK} from '../actions/index'
+
+export default function Reducer(state={}, action){
+
+    switch (action.type){
+        case LOAD_DECKS:
+            return{
+                ...state,
+                decks: action.decks
+            };
+        case ADD_DECK:
+            return{
+                ...state,
+                decks: [...state.decks, action.deck]
+            };
+        case ADD_CARD:
+            return{
+                ...state,
+                decks: state.decks.map((deck)=>{
+                  if(deck.id===action.deckId){
+                      deck.cards=[...deck.cards, action.card]
+                  }
+                  return deck;
+                })
+            };
+        default:
+            return state;
+    }
+}
